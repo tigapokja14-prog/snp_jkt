c4b0a3d54d56066c621c7abc9dc20aae5b221e5c
pokja3 <pokja3jkt@gmail.com>
2026-04-21 14:03:48 +0700

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-});
+
+  export default defineConfig({
+    output: "server",
+    adapter: netlify(),
+  });
