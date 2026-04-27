f08a4f114b6d05632034b0bf6e85622d67ce033b
pokja3 <pokja3jkt@gmail.com>
2026-04-27 17:46:08 +0700

    add vercel SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
-
+  output: 'server',
+  adapter: vercel(),
 });
