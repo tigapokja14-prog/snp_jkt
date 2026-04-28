a23a3d12541cb131e816c9f673c88b041fbedd17
pokja3 <pokja3jkt@gmail.com>
2026-04-28 10:04:48 +0700

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',  // ← kembali ke static
+  output: 'static',  // ← kembali ke static ya
 });
